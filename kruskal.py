7b9da3713a0f882a81d9a25a44ef32f94150c1f0
Raphael Sales <59770221+raphaelsales@users.noreply.github.com>
2024-07-04 11:38:37 -0300

    Add files via upload
    ALGORITIMO DE KRUSKAL

---
+++ kruskal.py
@@ -0,0 +1,51 @@
+class DisjointSet:
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.rank = [0] * n
+
+    def find(self, u):
+        if self.parent[u] != u:
+            self.parent[u] = self.find(self.parent[u])
+        return self.parent[u]
+
+    def union(self, u, v):
+        root_u = self.find(u)
+        root_v = self.find(v)
+        if root_u != root_v:
+            if self.rank[root_u] > self.rank[root_v]:
+                self.parent[root_v] = root_u
+            elif self.rank[root_u] < self.rank[root_v]:
+                self.parent[root_u] = root_v
+            else:
+                self.parent[root_v] = root_u
+                self.rank[root_u] += 1
+
+def kruskal(n, edges):
+    # Ordenar as arestas pelo peso
+    edges.sort(key=lambda edge: edge[2])
+    
+    ds = DisjointSet(n)
+    mst = []
+    total_cost = 0
+
+    for u, v, weight in edges:
+        if ds.find(u) != ds.find(v):
+            ds.union(u, v)
+            mst.append((u, v, weight))
+            total_cost += weight
+
+    return mst, total_cost
+
+# Exemplo de uso:
+n = 4  # Número de vértices
+edges = [
+    (0, 1, 10),
+    (0, 2, 6),
+    (0, 3, 5),
+    (1, 3, 15),
+    (2, 3, 4)
+]
+
+mst, total_cost = kruskal(n, edges)
+print("Árvore Geradora Mínima:", mst)
+print("Custo Total:", total_cost)
